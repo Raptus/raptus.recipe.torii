2460d71a0b322d9977b765a24928115d74f8c5d9
Raptus <dev@raptus.com>
2010-08-21 13:16:52 +0000

    adjust import path for torii template

--- raptus/recipe/torii/__init__.py
+++ raptus/recipe/torii/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import os, stat
 import raptus.torii
-
+import twisted
 """Recipe recipe"""
 
 
-REQUIRED_IMPORTS = [raptus.torii]
+REQUIRED_IMPORTS = [raptus.torii, twisted]
 
 class Recipe(object):
     """zc.buildout recipe"""
@@ -14,32 +14,36 @@ class Recipe(object):
         self.buildout, self.name, self.options = buildout, name, options
         self.buildout_var = buildout['buildout']
         self.torii_path = os.path.join(self.buildout_var['bin-directory'],options.name)
-        
         self.required_paths = []
         for req in REQUIRED_IMPORTS:
             path = req.__path__[0]
-            index = path.rfind(raptus.torii.__name__) + len(req.__name__)
-            self.required_paths.append(path[:index])
+            for i in range(req.__name__.count('.')+1):
+                path, tail = os.path.split(path)
+            self.required_paths.append(path)
         
+        self.instance = []
+        for part,opt in buildout.items():
+            if opt.has_key('recipe') and opt['recipe'] == 'plone.recipe.zope2instance' and not opt.has_key('zope-conf'):
+                self.instance.append(part)
+
+        for part in self.instance:
+            self.required_paths.append('%s/lib/python' % buildout[part]['zope2-location'])
+
         self.vars = dict(python_path = self.buildout_var['executable'],
                     raptus_torii_paths = "',\n'".join(self.required_paths),
                     socket_path = self.options['socket-path'])
 
         template = template_zope_conf % self.vars
-        self.instance = []
-        for part,opt in buildout.items():
-            if opt.has_key('recipe') and opt['recipe'] == 'plone.recipe.zope2instance' and not opt.has_key('zope-conf'):
-                self.instance.append(part)
         for part in self.instance:
             if buildout[part].has_key('zope-conf-additional'):
                 buildout[part]['zope-conf-additional'] += template
             else:
                 buildout[part]['zope-conf-additional'] = template
         if not self.instance:
-            """
-                 Warning: plone.recipe.zope2instace not found.
-                 zope.conf is not configured for torii.
-            """
+            print   """
+                         Warning: plone.recipe.zope2instace not found.
+                         zope.conf is not configured for torii.
+                    """
         
         
     def install(self):
